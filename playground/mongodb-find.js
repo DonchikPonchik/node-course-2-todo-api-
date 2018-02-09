b75165c02f24ad8615dd1eae7c937f28e33e89e5
Дониер <Donik@Donchik.local>
2018-02-09 14:33:50 +0500

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,41 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+var obj = new ObjectID();
+console.log(obj);
+
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+	if (err) {
+		console.log('Unable to connect to MongoDB server')
+	}
+
+	console.log('Connected to MongoDB server');
+
+
+
+	// db.collection('Todos').find({
+	// 	_id: new ObjectID('5a7c2dccf906de03d04dc0bc')
+	// }).toArray().then((docs) => {
+	// 	console.log('Todos');
+	// 	console.log(JSON.stringify(docs, undefined, 2));
+	// }, (err) => {
+	// 	console.log('Unable to fetch Todos', err);
+	// });
+
+
+	// db.collection('Todos').find().count().then((count) => {
+	// 	console.log(`Todos count: ${count}`);
+	// }, (err) => {
+	// 	console.log('Unable to fetch Todos', err);
+	// });
+
+
+	db.collection('Users').find({name: 'Donchik'}).toArray().then((docs) => {
+		console.log(JSON.stringify(docs, undefined, 2));
+	});
+
+
+	// db.close();
+});
